77ed8d698d35e71999d5c50e43bb6be5f0511c28
loxotan <144032714+loxotan@users.noreply.github.com>
2023-09-28 10:35:11 +0900

    230928 집

--- 7576 2.py
+++ 7576 2.py
@@ -1,5 +1,6 @@
 import sys
 input = sys.stdin.readline
+from collections import deque
 
 m, n = map(int, input().split())
 tomato = [[] for _ in range(n)]
@@ -7,22 +8,29 @@ for i in range(n):
     tomato[i] = list(map(int, input().split()))
     
 #bfs를 한 단계씩 시행해서 동시에 모든 토마토에서 하루씩 익어갈 수 있게
+q = deque([])
 
-def bfs(a, b): #(a,b)에서 d일째 시행
-    global keep_going
-    for i, j in [(1,0), (-1,0), (0, 1), (0,-1)]:
-        if 0<=a+i<n and 0<=b+j<m and tomato[a+i][b+j] == 0:
-            tomato[a+i][b+j] = 1 #주변 토마토 익힘
+def bfs():
+    while q:
+        a, b = q.popleft()
+        for io, jo in [(1,0), (-1,0), (0, 1), (0,-1)]:
+            if 0<=a+io<n and 0<=b+jo<m and tomato[a+io][b+jo] == 0:
+                tomato[a+io][b+jo] = tomato[a][b] + 1 #주변 토마토 익힘
+                q.append([a+io, b+jo])
 
-days = [tomato]
-while True:
-    for i in range(n):
-        for j in range(m):
-            if tomato[i][j] == 1:
-                bfs(i, j)
-    days.append(tomato)    
-    if days[-1] == days[-2]:
-        break
+for i in range(n):
+    for j in range(m):
+        if tomato[i][j] == 1:
+            q.append([i,j])
 
-print(len(days))
+bfs()
+res = 0
+for i in tomato:
+    for j in i:
+        if j == 0:
+            print(-1)
+            exit(0)
+    res = max(res, max(i))
+
+print(res-1)    
     
